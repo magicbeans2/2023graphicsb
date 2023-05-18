e144eea550bd1d80d5d00ce06d7e42846706cf82
magicbeans2 <a45629333@gmail.com>
2023-05-18 16:13:15 +0800

    week14

--- Final_Project/main.cpp
+++ Final_Project/main.cpp
@@ -6,16 +6,17 @@ GLMmodel*head=NULL;
 GLMmodel*body=NULL;
 GLMmodel*uparmR=NULL;
 GLMmodel*lowarmR=NULL;
-int show[4]={1,0,0,0};
+int show[4]={1,1,1,1};
+int ID=2;
 
 float teapotX=0,teapotY=0;
 FILE * fout =NULL;
 FILE * fin =NULL;
 void keyboard(unsigned char key,int x,int y){
-if(key=='0') show[0] =! show[0];
-if(key=='1') show[1] =! show[1];
-if(key=='2') show[2] =! show[2];
-if(key=='3') show[3] =! show[3];
+if(key=='0')ID=0; //show[0] =! show[0];
+if(key=='1')ID=1;  //show[1] =! show[1];
+if(key=='2')ID=2;  //show[2] =! show[2];
+if(key=='3')ID=3;  //show[3] =! show[3];
 glutPostRedisplay();
 }
 void display()
@@ -31,10 +32,21 @@ void display()
         glScalef(0.3,0.3,0.3);
         glPushMatrix();
             glTranslatef(teapotX,teapotY,0);
-            if(show[0])glmDraw(head,GLM_MATERIAL);
+        if(ID==0)glColor3f(1,0,0);
+        else glColor3f(1,1,1);
+        if(show[0])glmDraw(head,GLM_MATERIAL);
         glPopMatrix();
+
+        if(ID==1)glColor3f(1,0,0);
+        else glColor3f(1,1,1);
         if(show[1])glmDraw(body,GLM_MATERIAL);
+
+        if(ID==2)glColor3f(1,0,0);
+        else glColor3f(1,1,1);
         if(show[2])glmDraw(uparmR,GLM_MATERIAL);
+
+        if(ID==3)glColor3f(1,0,0);
+        else glColor3f(1,1,1);
         if(show[3])glmDraw(lowarmR,GLM_MATERIAL);
     glPopMatrix();
     glutSwapBuffers();
